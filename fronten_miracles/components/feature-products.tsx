4fc4628083cb696dbe650af57638916b36880867
Richard-Avrod <richimail003@gmail.com>
2026-01-09 02:35:58 -0600

    finish 7 jueves ene 2026

--- fronten_miracles/components/feature-products.tsx
+++ fronten_miracles/components/feature-products.tsx
@@ -6,10 +6,14 @@ import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel"
 import SkeletonSchema from "./skeleton-schema"
 import { ProductType } from "@/types/product"
 import { Card, CardContent } from "./ui/card"
+import { Expand, ShoppingCart } from "lucide-react"
+import IconButton from "./icon-buttons"
+import { useRouter } from "next/navigation"
+
 
 const FeatureProducts = () => {
     const {loading,result,error}: ResponseType = useGetFeaturedProducts()
-
+    const router = useRouter()
     console.log(result); 
     return (
         <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24 ">
@@ -22,7 +26,7 @@ const FeatureProducts = () => {
                     )}
                     {result !== null && (
                         result.map((product: ProductType) => {
-                            const { id, imagenes, nombreProducto, tipoProducto, costo } = product; 
+                            const { id, imagenes, nombreProducto, tipoProducto, costo, slug } = product; 
 
                             return(
                                 
@@ -33,7 +37,23 @@ const FeatureProducts = () => {
                                                 
                                                 <img 
                                                     src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${imagenes[0].url}`} 
-                                                    alt="image feature" /> 
+                                                    alt="image feature" />
+                                                <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5 ">
+                                                    <div className="flex justify-center gap-x-6"> 
+                                                        <IconButton 
+                                                        onClick={() => router.push(`product/${slug}`)}
+                                                        icon={<Expand size={20}/>}
+                                                        className="text-gray-600"
+                                                        />
+                                                        <IconButton 
+                                                        onClick={() => console.log("add item")}
+                                                        icon={<ShoppingCart size={20}/>}
+                                                        className="text-gray-600"
+                                                        />
+                                                        
+                                                         
+                                                    </div>
+                                                </div> 
                                             </CardContent>
                                         </Card>
                                     </div>
