c323880cc77a198d3c75c6d83f715bfb140c7658
Richard-Avrod <richimail003@gmail.com>
2026-04-02 12:12:58 -0600

    feat: configurar SMTP Gmail para emails de Strapi

--- backend-store-miracles/config/plugins.js
+++ backend-store-miracles/config/plugins.js
@@ -1,4 +1,22 @@
 module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: env('SMTP_USER'),
+          pass: env('SMTP_PASS'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_USER'),
+        defaultReplyTo: env('SMTP_USER'),
+      },
+    },
+  },
   upload: {
     config: {
       provider: '@strapi/provider-upload-cloudinary',
